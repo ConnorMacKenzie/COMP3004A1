3e3448e989060481ae9b2f353869b3b8d92b5c95
ConnorMacKenzie <connormack97@gmail.com>
2018-09-18 15:31:42 -0400

    Use mock deck to test splitting

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -15,13 +15,55 @@ public class PlayerTest extends TestCase {
 	
 	public void testSplit() {
 		Deck deck = new Deck();
+		deck.mockDeck();
+		
+		Card card1 = new Card("H", "2", 2);
+		Card card2 = new Card("D", "2", 2);
+		Card card3 = new Card("H", "4", 4);
+		Card card4 = new Card("H", "5", 5);
+		deck.mockAddCard(card1);
+		deck.mockAddCard(card2);
+		deck.mockAddCard(card3);
+		deck.mockAddCard(card4);
+		
 		Player player = new Player(deck);
-		assertEquals(50, deck.cardsInDeck());
+		
+		assertEquals(2, deck.cardsInDeck());
 		assertTrue(player.hand.isEmpty() == false);
 		assertTrue(player.hand2.isEmpty() == true);
+		
 		player.split();
+		
+		assertEquals(0, deck.cardsInDeck());
 		assertTrue(2 <= player.handTotal && player.handTotal <= 21);
 		assertTrue(2 <= player.handTotal2 && player.handTotal2 <= 21);
+		assertTrue(player.busted == false);
+	}
+	
+	public void testDoesNotSplitWithDiffCards() {
+		Deck deck = new Deck();
+		deck.mockDeck();
+		
+		Card card1 = new Card("H", "2", 2);
+		Card card2 = new Card("H", "3", 3);
+		Card card3 = new Card("H", "4", 4);
+		Card card4 = new Card("H", "5", 5);
+		deck.mockAddCard(card1);
+		deck.mockAddCard(card2);
+		deck.mockAddCard(card3);
+		deck.mockAddCard(card4);
+		
+		Player player = new Player(deck);
+		assertEquals(2, deck.cardsInDeck());
+		assertTrue(player.hand.isEmpty() == false);
+		assertTrue(player.hand2.isEmpty() == true);
+		
+		player.split();
+		
+		assertEquals(2, deck.cardsInDeck());
+		assertTrue(player.hand.isEmpty() == false);
+		assertTrue(player.hand2.isEmpty() == true);
+		assertTrue(2 <= player.handTotal && player.handTotal <= 21);
 		assertTrue(player.busted == false);
 	}
 	

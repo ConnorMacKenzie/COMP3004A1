e1a3e76ad28a824c6467bbcd71fcbc3d2dc89e44
ConnorMacKenzie <connormack97@gmail.com>
2018-09-18 14:31:36 -0400

    Update deck test with proper number of cards/shuffle test

--- src/test/java/core/DeckTest.java
+++ src/test/java/core/DeckTest.java
@@ -7,14 +7,14 @@ public class DeckTest extends TestCase {
 	public void testDeckAttributes() {
 		Deck deck = new Deck();
 		
-		assertEquals(56, deck.cardsInDeck());
+		assertEquals(52, deck.cardsInDeck());
 	}
 	
 	public void testDraw() {
 		Deck deck = new Deck();
 		Card card = deck.draw();
 		
-		assertEquals(55, deck.cardsInDeck());
+		assertEquals(51, deck.cardsInDeck());
 		assertTrue(1 <= card.getValue() && card.getValue() <= 11);
 	}
 	
@@ -23,7 +23,19 @@ public class DeckTest extends TestCase {
 		Card card = deck.draw();
 		Card card1 = deck.draw();
 		
-		assertEquals(54, deck.cardsInDeck());
+		assertEquals(50, deck.cardsInDeck());
 		assertTrue((card.getValue() != card1.getValue()) || (card.getSuit() != card1.getSuit()));
+	}
+	
+	public void testDeckIsShuffled() {
+		Deck deck = new Deck();
+		Card card = deck.draw();
+		Card card1 = deck.draw();
+		Card card2 = deck.draw();
+		
+		assertEquals(49, deck.cardsInDeck());
+		assertTrue(((card.getValue() != 1) && (card.getSuit() != "H"))
+					&& ((card1.getValue() != 2) && (card1.getSuit() != "H"))
+					&& ((card2.getValue() != 3) && (card2.getSuit() != "H")));
 	}
 }

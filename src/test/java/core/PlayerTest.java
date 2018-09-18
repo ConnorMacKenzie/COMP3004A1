1df7d594732e0d4498580b17ab1fa079a120c40d
ConnorMacKenzie <connormack97@gmail.com>
2018-09-18 14:36:28 -0400

    Fix number of cards in test

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -7,7 +7,7 @@ public class PlayerTest extends TestCase {
 	public void testPlayerAttributes() {
 		Deck deck = new Deck();
 		Player player = new Player(deck);
-		assertEquals(54, deck.cardsInDeck());
+		assertEquals(50, deck.cardsInDeck());
 		assertTrue(player.hand.isEmpty() == false);
 		assertTrue(2 <= player.handTotal && player.handTotal <= 21);
 		assertTrue(player.busted == false);
@@ -16,7 +16,7 @@ public class PlayerTest extends TestCase {
 	public void testSplit() {
 		Deck deck = new Deck();
 		Player player = new Player(deck);
-		assertEquals(54, deck.cardsInDeck());
+		assertEquals(50, deck.cardsInDeck());
 		assertTrue(player.hand.isEmpty() == false);
 		assertTrue(player.hand2.isEmpty() == true);
 		player.split();
